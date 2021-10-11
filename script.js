755d41233e566d20ab9baa3e80eae8ccaa585a80
J.Przydalski <przydal@gmail.com>
2021-10-11 16:09:31 +0200

    refactor code

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const startColor = [0, 245, 0];
 const endColor = [0, 0, 245];
-const FPS = 60;
+const FPS = 40;
 const radius = 50;
 
 var canvas;
 var ctx;
+var gradient;
 var ballY = 200;
 var step = 10;
-var currentColor = [0, 245, 0];
-var colorStep = 5;
-var gradient;
 
 window.onload = function () {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
-    gradient = ctx.createLinearGradient(0, 100, 200, 0);
+
+    gradient = ctx.createLinearGradient(0, 500, 0, 0);
     gradient.addColorStop(0, `rgb(${startColor[0]}, ${startColor[1]}, ${startColor[2]})`);
     gradient.addColorStop(1, `rgb(${endColor[0]}, ${endColor[1]}, ${endColor[2]})`);
     ctx.fillStyle = gradient;
+
     setInterval(() => {
         updatePositon();
         draw();
@@ -32,11 +32,10 @@ function draw() {
 }
 
 function updatePositon() {
-    ballY = ballY + step;
-    if (ballY >= canvas.height - radius) {
-        step = -step;
-    }
-    if (ballY <= 0 + radius) {
+    ballY += step;
+    if (
+        ballY >= canvas.height - radius || ballY <= 0 + radius
+    ) {
         step = -step;
     }
 }
